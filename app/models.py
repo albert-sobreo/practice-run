b296a64a69d07d547a172cb2730c591bf8a1f39d
Jan Albert Sobreo <janalbertsobreo@icloud.com>
2020-11-04 19:23:37 +0800

    updates

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post = models.CharField(max_length=1024)
+    upvote = models.IntegerField(default=0)
+    downvote = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'post'
+
+    def __str__(self):
+        return self.post
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        db_table = 'account'
+
+    def __str__(self):
+        return self.username
